525a80de675adbfd1df47cbbf76700791a44fb0d
spatulatom <spatulatom@gmail.com>
2022-11-26 21:23:37 +0000

    webpack config

--- website-projects/bookmark/js/script.js
+++ website-projects/bookmark/js/script.js
@@ -1,36 +1,10 @@
-const tabs = document.querySelectorAll('.tab')
-const panels = document.querySelectorAll('.panel')
+// 1. Menu:
 const btn = document.getElementById('menu-btn')
 const menu = document.getElementById('menu')
 const logo = document.getElementById('logo')
 
-// Tabs menu event listener
-tabs.forEach((tab) => tab.addEventListener('click', onTabClick))
-
 // Hamburger button listener
 btn.addEventListener('click', navToggle)
-
-function onTabClick(e) {
-  // Deactivate all tabs
-  tabs.forEach((tab) => {
-    tab.children[0].classList.remove(
-      'border-softRed',
-      'border-b-4',
-      'md:border-b-0'
-    )
-  })
-
-  // Hide all panels
-  panels.forEach((panel) => panel.classList.add('hidden'))
-
-  // Activate a new tab and panel based on the target
-  e.target.classList.add('border-softRed', 'border-b-4')
-  const classString = e.target.getAttribute('data-target')
-  document
-    .getElementById('panels')
-    .getElementsByClassName(classString)[0]
-    .classList.remove('hidden')
-}
 
 function navToggle() {
   btn.classList.toggle('open')
@@ -42,4 +16,40 @@ function navToggle() {
   } else {
     logo.setAttribute('src', './images/logo-bookmark.svg')
   }
+}
+
+// 2. Features section:
+
+const tabs = document.querySelectorAll('.tab')
+// the line above creates something called node list which is similar to ana array
+// now when we want to get elemenst from that list we use notation like with arrays
+const panels = document.querySelectorAll('.panel')
+
+// Tabs menu event listener
+tabs.forEach((tab) => tab.addEventListener('click', onTabClick))
+
+
+function onTabClick(e) {
+  // Deactivate all tabs (+borders)
+  tabs.forEach((tab) => {
+    console.log(tab.children[0],'accesing children of a tab becasue on them is a class we want to remove')
+    tab.children[0].classList.remove(
+      'border-softRed' 
+    )
+  })
+
+  // Hide all panels
+  panels.forEach((panel) => panel.classList.add('hidden'))
+
+  // Activate a new tab and panel based on the data-target attribute of a tab, and 
+  // corrensponding class of a panel
+  console.log('e.target', e.target)
+  // why this is pointing to the children of a tab? not to the tab itself?
+  e.target.classList.add('border-softRed', 'border-b-4')
+  const classString = e.target.getAttribute('data-target')
+  console.log('classString', classString)
+  document
+    .getElementById('panels')
+    .getElementsByClassName(classString)[0]
+    .classList.remove('hidden')
 }
